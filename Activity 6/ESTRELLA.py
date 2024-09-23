008995e714a043688b0a7e66e0385a33a4bc3512
EstrellaMichaellaMaeM <ronnieemily2317@gmail.com>
2024-09-23 13:04:58 +0800

    Create ESTRELLA.py

---
+++ Activity 6/ESTRELLA.py
@@ -0,0 +1,17 @@
+name = input("Michaella: ")
+age = input("19: ")
+# Use an f-string to format the output
+print(f"Hello, Michaella! You are 19 years old.")
+
+
+item = "grapes"
+count = 8
+# Use the .format() method
+sentence = "I bought 8 grapes today.".format(count, item)
+print(sentence)
+
+
+city = ("Switzerland: ")
+temperature = 14
+# Use the % operator
+print("The temperature in Switzerland is 14 degrees Celsius.")
